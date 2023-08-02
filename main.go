e27bb2133a38436b236c3cef5550842a6b934c49
Vitor Wdson <vitor.wdson2@gmail.com>
2023-08-01 20:37:20 -0400

    Working with template inheritance

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var TemplateFolder = "./templates/"
+
+func getTemplate(templatePath string) (*template.Template, error) {
+    files := []string{
+        "./templates/base.html",
+        "./templates/" + templatePath,
+    }
+
+    log.Println(files)
+
+    tmpl, err := template.ParseFiles(files...)
+    if err != nil {
+        return nil, err
+    }
+
+	return tmpl, nil 
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := getTemplate("index.html")
+        if err != nil {
+            log.Print(err.Error())
+            http.Error(w, "Error loading template", 500)
+        }
+
+        err = tmpl.ExecuteTemplate(w, "base", map[string]string{
+			"Name": "test",
+		})
+        if err != nil {
+            log.Print(err.Error())
+            http.Error(w, "Error executing template", 500)
+        }
+	})
+
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
+	http.ListenAndServe(":3000", nil)
 }
