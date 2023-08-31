6603877ef8896db994f114509b5243ec51be493f
Vitor Wdson <vitor.wdson2@gmail.com>
2023-08-30 20:45:22 -0400

    Added field validation for the User model

--- models/user/validate.go
+++ models/user/validate.go
@@ -1,6 +1,29 @@
 package user
 
+import "errors"
+
 func (u User) Validate() error {
-    // TODO: Validate user maybe using database connection, not sure yet
+    if u.Name != "" {
+        return errors.New("The user does not have a name")
+    }
+
+    if u.Username != "" {
+        return errors.New("The user does not have an username")
+    }
+    if len(u.Username) > 30 {
+        return errors.New("The username is too big (max: 30)")
+    }
+
+    if u.password != "" {
+        return errors.New("The user does not have a password")
+    }
+
+    if u.Nickname != "" {
+        return errors.New("The user does not have a nickname")
+    }
+    if len(u.Nickname) > 40 {
+        return errors.New("The nickname is too big (max: 40)")
+    }
+
     return nil
 }
