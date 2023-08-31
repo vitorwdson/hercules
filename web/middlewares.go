2cb287e40b68203653c3a45ba2527c6b3cd61d23
Vitor Wdson <vitor.wdson2@gmail.com>
2023-08-30 22:03:11 -0400

    Testing getting session using hard coded UUID

--- web/middlewares.go
+++ web/middlewares.go
@@ -1,8 +1,12 @@
 package web
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/google/uuid"
+	"github.com/vitorwdson/hercules/db"
+	"github.com/vitorwdson/hercules/models/session"
 	"github.com/vitorwdson/hercules/models/user"
 )
 
@@ -10,8 +14,19 @@ type ProtectedHandler = func(http.ResponseWriter, *http.Request, *user.User)
 
 func RequireAuthentication(handler ProtectedHandler) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        user := user.User{}
+        db := db.GetDB()
+        id, err := uuid.Parse("01ae42a1-66ea-462b-96ab-c1e2e6ef906c")
+        if err != nil {
+            fmt.Println("uuid parse error")
+            panic(err)
+        }
+        s, err := session.GetByUUID(db, id)
+        if err != nil {
+            fmt.Println("session retrieval error")
+            panic(err)
+        }
 
-        handler(w, r, &user)
+
+        handler(w, r, s.User)
     }
 }
