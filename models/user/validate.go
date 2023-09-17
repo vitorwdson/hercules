54ce7d8515f668e943c0f91c0e1a4379568132f6
Vitor Wdson <vitor.wdson2@gmail.com>
2023-09-17 03:57:05 -0400

    Fixed model validation

--- models/user/validate.go
+++ models/user/validate.go
@@ -5,22 +5,22 @@ import (
 )
 
 func (u User) Validate() error {
-	if u.Name != "" {
+	if u.Name == "" {
 		return errors.New("The user does not have a name")
 	}
 
-	if u.Username != "" {
+	if u.Username == "" {
 		return errors.New("The user does not have an username")
 	}
 	if len(u.Username) > 30 {
 		return errors.New("The username is too big (max: 30)")
 	}
 
-	if u.ID == 0 && u.password != "" {
+	if u.ID == 0 && u.password == "" {
 		return errors.New("The user does not have a password")
 	}
 
-	if u.Nickname != "" {
+	if u.Nickname == "" {
 		return errors.New("The user does not have a nickname")
 	}
 	if len(u.Nickname) > 40 {
